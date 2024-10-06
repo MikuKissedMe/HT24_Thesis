647a7fa19247281d0b3487c250b1d4dcf3f3fe96
HL <haolin96@msn.cn>
2024-10-06 18:40:13 +0200

    PIC V2
    with venv fully setuped

--- code/main.py
+++ code/main.py
@@ -11,6 +11,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 print("Current working directory:", os.getcwd())
 
+'''
 def PIC(tree):
     # The PIC algorithm should only collapse leafs for n - 1 times, i.e. (total_leaf - 1) times
     total_leaf = len(tree.get_terminals())
@@ -38,16 +39,17 @@ def PIC(tree):
     print(tree)
     print(tree.get_terminals())
 
+    
 def add_length(leaf_i,leaf_j):
-    ''' Currently only computing branch length'''
+    # Currently only computing branch length
     if (float(leaf_i.branch_length) == 0) and (float(leaf_j.branch_length) == 0):
         return float(0)
     else:
         return (float(leaf_i.branch_length) * float(leaf_j.branch_length))/(float(leaf_i.branch_length) + float(leaf_j.branch_length))
 
-
+        
 def collapse_leaf(leaf_i,leaf_j,LCA):
-    ''' Returns boolean that shows if leafs are collapsed'''
+    # Returns boolean that shows if leafs are collapsed
     if leaf_i in LCA and leaf_j in LCA:
         # If the last common ancestor are also direkt ancestor to leafs
 
@@ -67,15 +69,67 @@ def collapse_leaf(leaf_i,leaf_j,LCA):
         # The last common ancestor are not direkt ancestor for some leaf
         return False
 
+'''
+
+
+def PIC_new(tree):
+    child_list = []
+    for child in tree.clade:
+        child_list.append(child)
+    for child in child_list:
+        traverse(child)
+
+
+def traverse(clade):
+    # if a node has only 2 child and both are leafs then collapse it
+    if clade.is_preterminal() and len(clade) == 2:
+        collapse_parent(clade)
+    else: # else traverse to child if there are any
+        if len(clade) > 0:
+            child_list = []
+            for child in clade:
+                child_list.append(child)
+            for child in child_list:
+                traverse(child)
+
+        # collapse the clade when children of it are traversed and collapsed
+        if clade.is_preterminal() and len(clade) == 2:
+            collapse_parent(clade)
+
+def add_length(leaf_i,leaf_j):
+    # Currently only computing branch length
+    if (float(leaf_i.branch_length) == 0) and (float(leaf_j.branch_length) == 0):
+        return float(0)
+    else:
+        return (float(leaf_i.branch_length) * float(leaf_j.branch_length))/(float(leaf_i.branch_length) + float(leaf_j.branch_length))
+
+def collapse_parent(clade):
+    leaf_list = []
+
+    new_branch_length = float(0)
+    for child in clade:
+        leaf_list.append(child)
+
+    new_branch_length += add_length(leaf_list[0],leaf_list[1])
+    clade.branch_length = float(clade.branch_length) + float(new_branch_length)
+
+    for child in leaf_list:
+        tree.collapse(child)
+    
 
 
 tree = Phylo.read("PS00027.treefile", "newick")
 #tree = Phylo.read("PS00673_full.phy.treefile", "newick")
 print(tree)
+print(tree.count_terminals())
 
 
 start_time = time.time()
 
-PIC(tree)
+#PIC(tree)
+PIC_new(tree)
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
+print(tree)
+print(tree.count_terminals())
