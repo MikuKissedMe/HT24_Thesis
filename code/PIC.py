db1ca17ed7cab90e9c1438bb01cf0ce17f4d3936
HL <haolin96@msn.cn>
2025-03-18 15:00:10 +0100

    Create exempel data for drawlogo

--- code/PIC.py
+++ code/PIC.py
@@ -18,7 +18,12 @@ from pyseqlogo.pyseqlogo import draw_logo, setup_axis
 
 #Suppress runtime warnings caused by /pyseqlogo/format_utils.py
 #warnings.filterwarnings("ignore", category=RuntimeWarning)
-
+def export_scores_to_file(scores_str,file_name):
+    try:
+        with open(file_name, "x") as f:
+            f.write(scores_str)
+    except FileExistsError:
+        print("Already exists.")
 
 def convert_matrix_to_scores(matrix):
     '''convert the seq_counter matrix to the format used by pyseqlogo '''
@@ -36,6 +41,7 @@ def convert_matrix_to_scores(matrix):
         for c in config.existing_characters:
             scores.append((str(c),matrix[config.charaters.index(c)][i]))
         all_scores.append(scores)
+    export_scores_to_file(str(all_scores),'with_PIC.txt')
     return all_scores
 
 def convert_count_to_scores(matrix):
@@ -46,7 +52,7 @@ def convert_count_to_scores(matrix):
         for key, value in matrix.items():
             position_scores.append((str(key),float(value[i]/config.terminals)))
         all_scores_count.append(position_scores)
-
+    export_scores_to_file(str(all_scores_count),'without_PIC.txt')
     return all_scores_count
 
 def set_seq(leaf_i,leaf_j):
@@ -241,11 +247,77 @@ def main():
     plt.rcParams['figure.dpi'] = 300
     fig, axarr = draw_logo(convert_count_to_scores(config.seq_counter),data_type='bits',seq_type=config.seq_type, yaxis='bits',colorscheme='hydrophobicity',draw_axis=True)
     fig.tight_layout()
-    fig.savefig("without_PIC_bits_bits.png")
+    fig.savefig("without_PIC_bits.png")
     print("without_PIC_bits.png saved")
 
     print("---Done in %s seconds ---" % round(time.time() - start_time))
 
+    ALL_SCORES1 = [[('C', 0.02247014831444764),
+              ('T', 0.057903843733384308),
+              ('A', 0.10370837683591219),
+              ('G', 0.24803586793255664)],
+             [('T', 0.046608227674354567),
+              ('G', 0.048827667087419063),
+              ('A', 0.084338697696451109),
+              ('C', 0.92994511407402669)],
+             [('G', 0.0),
+              ('T', 0.011098351287382456),
+              ('A', 0.022196702574764911),
+              ('C', 1.8164301607015951)],
+             [('C', 0.020803153636453006),
+              ('T', 0.078011826136698756),
+              ('G', 0.11268374886412044),
+              ('A', 0.65529933954826969)],
+             [('T', 0.017393530660176126),
+              ('A', 0.030438678655308221),
+              ('G', 0.22611589858228964),
+              ('C', 0.45078233627623127)],
+             [('G', 0.022364103549245576),
+              ('A', 0.043412671595594352),
+              ('T', 0.097349627214363091),
+              ('C', 0.1657574733649966)],
+             [('C', 0.03264675899941203),
+              ('T', 0.045203204768416654),
+              ('G', 0.082872542075430544),
+              ('A', 1.0949220710572034)],
+             [('C', 0.0),
+              ('T', 0.0076232429756614498),
+              ('A', 0.011434864463492175),
+              ('G', 1.8867526364762088)],
+             [('C', 0.0018955903000026028),
+              ('T', 0.0094779515000130137),
+              ('A', 0.35637097640048931),
+              ('G', 0.58005063180079641)],
+             [('A', 0.01594690817903021),
+              ('C', 0.017541598996933229),
+              ('T', 0.2774762023151256),
+              ('G', 0.48638069946042134)],
+             [('A', 0.003770051401807444),
+              ('C', 0.0075401028036148881),
+              ('T', 0.011310154205422331),
+              ('G', 1.8624053924928772)],
+             [('C', 0.036479877757360731),
+              ('A', 0.041691288865555121),
+              ('T', 0.072959755514721461),
+              ('G', 1.1517218549109602)],
+             [('G', 0.011831087684038642),
+              ('T', 0.068620308567424126),
+              ('A', 0.10174735408273231),
+              ('C', 1.0009100180696691)],
+             [('C', 0.015871770937774379),
+              ('T', 0.018757547471915176),
+              ('A', 0.32176408355669878),
+              ('G', 0.36505073156881074)],
+             [('A', 0.022798100897300954),
+              ('T', 0.024064662058262118),
+              ('G', 0.24571286522646588),
+              ('C', 0.34070495229855319)]]
+    
+    plt.rcParams['figure.dpi'] = 300
+    fig, axarr = draw_logo(ALL_SCORES1)
+    fig.tight_layout()
+    fig.savefig('seq_ex.png')
+    print('seq_ex.png saved')
 
 if __name__ == "__main__":
     try:
